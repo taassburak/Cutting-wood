3d9dca1cb8b267df4c9f070757ddb1860ea526e3
taassburak <56767932+taassburak@users.noreply.github.com>
2020-09-27 03:25:02 +0300

    cutting wood 3d game

---
+++ cutting wood/cutting wood/Assets/scripts/FORCE.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FORCE : MonoBehaviour
+{
+    Rigidbody fizik;
+    public float z_axis=-500f;
+    public float y_axis = -30;
+    float forcetime = 0;
+    public float waitingtime = 0.7f;
+    
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody>();
+        fizik.AddForce(0, y_axis, z_axis);
+
+    }
+    void Update()
+    {
+        kuvvet();
+    }
+    void kuvvet()
+    {
+        
+            forcetime += Time.deltaTime;
+            if (forcetime > waitingtime)
+            {
+                fizik.AddForce(0, y_axis / 2, z_axis / 2);
+                forcetime = 0;
+            }
+        
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag=="zemin2")
+        {
+            Destroy(gameObject,2);
+        }
+    }
+
+}
---
+++ cutting wood/cutting wood/Assets/scripts/kameraShake.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kameraShake : MonoBehaviour
+{
+    public IEnumerator Shake (float time, float mag)
+    {
+        Vector3 originalPos = transform.localPosition;
+
+        float shakeTime = 0.0f;
+
+        while (shakeTime < time)
+        {
+            float x = Random.Range(-1f, 1f) * mag;
+            float y = Random.Range(-1f, 1f) * mag;
+
+            transform.localPosition = new Vector3(x, y, originalPos.z);
+
+            shakeTime += Time.deltaTime;
+
+            yield return null;
+        }
+        transform.localPosition = originalPos;
+
+
+    }
+}
